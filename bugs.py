ef2a716b2f3f25dbbf4c9bbc1fb1cdbc2d221f6b
tungvx <toilatung90@gmail.com>
2016-09-15 17:56:01 +0200

    updating pbs

--- bugs.py
+++ bugs.py
@@ -5,7 +5,8 @@ rank = comm.Get_rank()
 size = comm.Get_size()
 
 if rank == 0:
-	for proc_rank in range(size-1) + 1:
+	for i in range(size-1):
+		proc_rank = i + 1
 		comm.isend([str(proc_rank),str(proc_rank),str(proc_rank),str(proc_rank),str(proc_rank),str(proc_rank),str(proc_rank),str(proc_rank),str(proc_rank)], proc_rank)
 else:
 	print ("Rank", rank, "receiving", comm.recv(source=0))
