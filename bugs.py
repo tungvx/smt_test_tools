9dc1dfe372e1ecf839677335bde993ea21ff4e35
tungvx <toilatung90@gmail.com>
2016-09-15 14:33:12 +0200

    updating pbs

---
+++ bugs.py
@@ -0,0 +1,11 @@
+from mpi4py import MPI
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+
+if rank == 0:
+	for proc_rank in range(size-1) + 1:
+		comm.isend([str(proc_rank),str(proc_rank),str(proc_rank),str(proc_rank),str(proc_rank),str(proc_rank),str(proc_rank),str(proc_rank),str(proc_rank)], proc_rank)
+else 
+	print ("Rank", rank, "receiving", comm.recv(source=0))
