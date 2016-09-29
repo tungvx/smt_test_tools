0030230b36456e2a76e4b63e9607fc805f007b08
tungvx <toilatung90@gmail.com>
2016-09-29 15:14:37 +0200

    fix bugs infinity in AA

--- run.py
+++ run.py
@@ -168,5 +168,5 @@ def run(tool, directory, timeout, resultFile, PROCESSES_NUM, SOLVED_PROBLEM, max
 # run("../veriT", "../test", 30, "veriT.csv", 4, SMT2, 40000, "--disable-banner --disable-print-success")
 # run("z3", "Test/test", 30, "z3.csv", 4, SMT2, 1000000)
 # run("veriT_reduce", "test", 20, "veriT_reduce.csv", 4, SMT2, 400000, "--disable-banner --disable-print-success")
-# run("veriT_reduce", "/home/tungvx/raSAT/development_ver/raSAT/Test/smtlib-20140121/QF_NRA", 30, "veriT_reduce.csv", 4, SMT2, 400000, "--disable-banner --disable-print-success")
+# run("veriT", "/home/tungvx/raSAT/development_ver/raSAT/Test/smtlib-20140121/QF_NRA/meti-tarski/exp/problem/10/3/weak/", 30, "veriT.csv", 4, SMT2, 400000, "--disable-banner --disable-print-success")
 # run("veriT_raSAT", "test", 30, "veriT_raSAT.csv", 4, SMT2, 400000, "--disable-banner --disable-print-success")
